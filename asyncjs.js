5106a504eae343147cc4d43833d202426a6e056f
Shubham <40598861+vijit101@users.noreply.github.com>
2024-10-28 17:59:47 +0530

    added set interval test
    its like invoke repeating in unity

--- asyncjs.js
+++ asyncjs.js
@@ -1,3 +1,9 @@
 console.log("called normally");
 setTimeout(()=>{console.log("testing async code ")},5000);
 console.log("called after set timeout");
+
+// set interval
+
+setInterval(() => {
+    console.log("test set Interval");
+}, 6000);
